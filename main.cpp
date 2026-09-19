de7c7900c61973422870fca2c38a6887dd9e68b1
Adelina Brukaitė <adelina.brukaite@gmail.com>
2026-09-19 18:54:34 +0300

    baigtas duomenu ivedimas

--- main.cpp
+++ main.cpp
@@ -1,9 +1,23 @@
 #include <iostream>
-
-using namespace std;
-
+#include <vector>
+#include <iomanip>
+#include <string>
+struct studentas {
+    std::string vardas, pavarde;
+    std:vector<int> pazymys;
+    int egzaminas;
+};
 int main()
 {
-    cout << "Hello world!" << endl;
-    return 0;
+   studentas a;
+   std::cout<<"Iveskite studento varda: "; cin>>A.vardas;
+   std::cout<<"Iveskite studento pavarde: "; cin>>A.pavarde;
+   while(true){
+    int n; char klausimas;
+    std::cout<<"Iveskite namu darbu pazymi :"; cin>>A.pazymys;
+    std::cout<<"Ar studentas dar turi pazymiu? (t/n) "; cin>>klausimas;
+    if(klausimas == "n" || klausimas == "N") break;
+   }
+   std::cout<<"Iveskite studento egzamino rezultata: "; cin>>A.egzaminas
+
 }
