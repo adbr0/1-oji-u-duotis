60c0238aafc29a72c82cd0d9bf97874558c7be51
Adelina Brukaitė <adelina.brukaite@gmail.com>
2026-09-20 16:43:01 +0300

    isvedimas

--- main.cpp
+++ main.cpp
@@ -12,12 +12,12 @@ int main()
    studentas A;
    std::cout<<"Iveskite studento varda: "; std::cin>>A.vardas;
    std::cout<<"Iveskite studento pavarde: "; std::cin>>A.pavarde;
-   int b=0;
+   int bendras=0;
    while(true){
-    int n; char klausimas;
-    std::cout<<"Iveskite namu darbu pazymi :"; std::cin>>n;
-    A.pazymys.push_back(n);
-    b+=n;
+    int l; char klausimas;
+    std::cout<<"Iveskite namu darbu pazymi :"; std::cin>>l;
+    A.pazymys.push_back(l);
+    bendras+=l;
     std::cout<<"Ar studentas dar turi pazymiu? (t/n) "; std::cin>>klausimas;
     if (klausimas == 'n' || klausimas == 'N') {
     break;
@@ -25,7 +25,13 @@ int main()
 
    }
    std::cout<<"Iveskite studento egzamino rezultata: "; std::cin>>A.egzaminas;
-   std::cout<<b;
+
+   std::cout<<"Pavarde " << " Vardas " << " Galutinis (Vid.)"<< std::endl;
+   //std::cout<<"-"*;
+   float galutinis;
+   galutinis = 0.4*(bendras/A.pazymys.size()) + 0.6*A.egzaminas;
+   std::cout<<A.pavarde<<A.vardas<<galutinis;
+
 
 
 }
