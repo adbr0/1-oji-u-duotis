ec51e02390eb699022cfe3287cc17276e605ed7b
Adelina Brukaitė <adelina.brukaite@gmail.com>
2026-09-23 13:11:53 +0300

    prideta mediana

--- main.cpp
+++ main.cpp
@@ -2,6 +2,7 @@
 #include <vector>
 #include <iomanip>
 #include <string>
+#include <algorithm>
 struct studentas {
     std::string vardas, pavarde;
     std::vector<int> pazymys;
@@ -24,15 +25,36 @@ int main()
     }
 
    }
+   double mediana;
+   double galutinis;
+    std::sort(A.pazymys.begin(),A.pazymys.end());
+    if (A.pazymys.size()%2==0)
+    {
+        mediana=(A.pazymys[A.pazymys.size()/2-1]+A.pazymys[A.pazymys.size()/2])/2.0;
+    }
+    else
+    {
+        mediana=A.pazymys[A.pazymys.size()/2];
+    }
+    char klausimas;
    std::cout<<"Iveskite studento egzamino rezultata: "; std::cin>>A.egzaminas;
+   std::cout<<"Skaiciuoti studento vidurki (t/n)?"; std::cin>>klausimas;
    std::cout << "" << std::left<< std::setw(10) << "Pavarde" <<""<<std::left<< std::setw(10) << "Vardas" << "";
-   std::cout << std::right << std::setw(5) << "Galutinis (Vid.)" << "\n";
+
+
+    if (klausimas == 'n' || klausimas == 'N') {
+        std::cout << std::right << std::setw(5) << "Galutinis (Med.)" << "\n";
+        galutinis = 0.4 * mediana + 0.6 * A.egzaminas;
+    }
+    else{
+        std::cout << std::right << std::setw(5) << "Galutinis (Vid.)" << "\n";
+        galutinis = 0.4 * (bendras/A.pazymys.size()) + 0.6 * A.egzaminas;
+    }
+
    for(int i = 0; i < 34; i++) {
     std::cout << "-";
     }
     std::cout << "\n";
-    float galutinis;
-    galutinis = 0.4 * (bendras / A.pazymys.size()) + 0.6 * A.egzaminas;
     std::cout << "" << std::left<< std::setw(10)<< A.pavarde <<""<<std::left<< std::setw(10) << A.vardas <<"";
     std::cout << std::right <<std::setw(5) << std::fixed << std::setprecision(2) << galutinis <<"";
 
